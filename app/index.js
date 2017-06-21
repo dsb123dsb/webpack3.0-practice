86c0a82dddabe5b86e8db41d783677c04bdc6113
dssb123dsb <523598438@qq.com>
2017-06-21 13:01:31 +0800

    init,traditional project dependency传统的项目依赖

---
+++ app/index.js
@@ -0,0 +1,10 @@
+function componennt(){
+	var element = document.createElement('div');
+
+	/* 需要引入lodash，下一行才能正常工作*/
+	element.innerHTML = _.join(['hello', 'webpack', ' ']);
+
+	return element;
+}
+
+document.body.appendChild(componennt());
